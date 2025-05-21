b28a331e8fb6d3113e4355a4d8da5ed79f9093c6
Lazyihui <chengbaozi6@qq.com>
2025-05-21 20:47:45 +0800

    <feature>add: 协议

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace MyTelepathy {
+    public class MyTelepathy {
+        
+        // public static void Main(string[] args) {
+        //     Console.WriteLine("Hello, World!");
+        // }
+
+        public void Start() {
+            Console.WriteLine("Starting MyTelepathy...");
+        }
+
+    }
+}
