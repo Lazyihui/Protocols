141cb57f4e9699d5053feae771e7abc924df8dd8
Lazyihui <chengbaozi6@qq.com>
2025-05-23 21:56:59 +0800

    <feature>add; BelongName

--- Protocols/SpawnBullet/SpawnBulletReqMessage.cs
+++ Protocols/SpawnBullet/SpawnBulletReqMessage.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 namespace MyTelepathy {
 
     public struct SpawnBulletReqMessage {
+        public string belongName;
         public Transform rootPos;
         public Vector3 pos;
     }
